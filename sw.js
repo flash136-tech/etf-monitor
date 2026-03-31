45f33458f0f2b755d63a76168bdc18333f7f90b6
flash136-tech <flash136@gmail.com>
2026-03-31 23:06:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pledge-v3';
+const CACHE = 'pledge-v4';
 const FILES = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
